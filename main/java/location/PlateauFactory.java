f2849db11ac144a5ac5e344e419aeae68709d513
othman853 <anuar.yasser@gmail.com>
2016-12-02 09:08:14 -0200

    Adding a plateauFactory

---
+++ main/java/location/PlateauFactory.java
@@ -0,0 +1,12 @@
+package location;
+
+public class PlateauFactory {
+
+    private static final int DEFAULT_HORIZONTAL_SIZE = 10;
+    private static final int DEFAULT_VERTICAL_SIZE = 10;
+
+    public Plateau createDefaultPlateau() {
+        return new Plateau(DEFAULT_HORIZONTAL_SIZE, DEFAULT_VERTICAL_SIZE);
+    }
+
+}
---
+++ test/java/location/PlateauFactoryTest.java
@@ -0,0 +1,21 @@
+package location;
+
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class PlateauFactoryTest {
+
+    PlateauFactory factory = new PlateauFactory();
+
+    @Test
+    public void defaultShouldCreateA10by10Plateau() throws Exception {
+
+        Plateau plateau = factory.createDefaultPlateau();
+
+        assertEquals(10, plateau.horizontalBoundary);
+        assertEquals(10, plateau.verticalBoundary);
+
+    }
+}
