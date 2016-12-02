35c17fe1a10f3c4b03280cca9f3c8f36f9edcbd6
othman853 <anuar.yasser@gmail.com>
2016-12-02 19:28:23 -0200

    Removing old code

--- main/java/MarsRovers.java
+++ main/java/MarsRovers.java
@@ -1,15 +1,8 @@
-import location.Plateau;
-import location.PlateauFactory;
-import location.Position;
 
 public class MarsRovers {
 
     public static void main(String[] args) {
 
-
-        Plateau plateau = new PlateauFactory().createDefaultPlateau();
-
-        plateau.add(new Position(1,1));
 
     }
 
