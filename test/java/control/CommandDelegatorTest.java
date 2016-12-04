30ada2d5d48b2af8efd57e360a397a3f73375985
othman853 <anuar.yasser@gmail.com>
2016-12-04 15:24:21 -0200

    Adding more test cases to command delegator

--- test/java/control/CommandDelegatorTest.java
+++ test/java/control/CommandDelegatorTest.java
@@ -1,6 +1,9 @@
 package control;
 
+import location.Orientation;
+import location.Position;
 import org.junit.Test;
+import units.Rover;
 
 import static org.junit.Assert.*;
 import static control.CommandDelegator.*;
@@ -20,5 +23,29 @@ public class CommandDelegatorTest {
     @Test(expected = IllegalArgumentException.class)
     public void shouldThrowExceptionWhenCommandIsNotFound() throws Exception {
         fromString("Whatever");
+    }
+
+    @Test
+    public void delegateShouldMoveRoverRight() throws Exception {
+        Rover rover = new Rover("a", Orientation.NORTH, new Position(1,1));
+        Rover movedRover = RIGHT.delegate(rover);
+
+        assertEquals(Orientation.EAST, movedRover.orientation);
+    }
+
+    @Test
+    public void delegateShouldMoveRoverLeft() throws Exception {
+        Rover rover = new Rover("a", Orientation.NORTH, new Position(1,1));
+        Rover movedRover = LEFT.delegate(rover);
+
+        assertEquals(Orientation.WEST, movedRover.orientation);
+    }
+
+    @Test
+    public void delegateShouldMoveRoverForward() throws Exception {
+        Rover rover = new Rover("a", Orientation.NORTH, new Position(1,1));
+        Rover movedRover = MOVE.delegate(rover);
+
+        assertEquals(2, movedRover.position.y);
     }
 }
