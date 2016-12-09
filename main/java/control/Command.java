4e980b1bf365475452e7c32716445b81f69ea524
othman853 <anuar.yasser@gmail.com>
2016-12-09 12:21:30 -0200

    Using static imports on Command

--- main/java/control/Command.java
+++ main/java/control/Command.java
@@ -1,29 +1,29 @@
 package control;
 
-
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
+
+import static java.util.Arrays.asList;
+import static java.util.Collections.emptyList;
 
 public class Command {
 
     private static final String VALID_COMMAND_REGEX = "[LMR]+";
     private static final String EMPTY_REGEX = "";
 
-    public final String commandString;
+    private final String commandString;
 
     public Command(String commandString) {
         this.commandString = commandString;
     }
 
-    public boolean isExecutable() {
+    boolean isExecutable() {
         return commandString != null && commandString.matches(VALID_COMMAND_REGEX);
     }
 
-    public List<String> getMoveList() {
+    List<String> getMoveList() {
         return isExecutable()
-                ? Arrays.asList(commandString.split(EMPTY_REGEX))
-                : Collections.emptyList();
+                ? asList(commandString.split(EMPTY_REGEX))
+                : emptyList();
     }
 
 }
