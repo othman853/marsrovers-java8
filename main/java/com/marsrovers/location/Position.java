97d219a71d087bcc15e7cc9cc869289a80d2cdee
othman853 <anuar.yasser@gmail.com>
2016-12-12 16:03:52 -0200

    Implementing equals and hashcode for position

--- main/java/com/marsrovers/location/Position.java
+++ main/java/com/marsrovers/location/Position.java
@@ -22,4 +22,22 @@ public class Position {
     public String toString() {
         return x + ", " +  y;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Position position = (Position) o;
+
+        if (x != position.x) return false;
+        return y == position.y;
+    }
+
+    @Override
+    public int hashCode() {
+        int result = x;
+        result = 31 * result + y;
+        return result;
+    }
 }
